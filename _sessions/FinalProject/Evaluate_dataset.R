26c899c096dd352ba3a945145fc66b7e642bfaf9
Dirk Wulff <dirk.wulff@gmail.com>
2019-02-03 11:15:41 +0100

    dunno

--- _sessions/FinalProject/Evaluate_dataset.R
+++ _sessions/FinalProject/Evaluate_dataset.R
@@ -3,50 +3,52 @@
 
 # packages
 library(tidyverse)
-library("tidylog", warn.conflicts = FALSE)
+#library("tidylog", warn.conflicts = FALSE)
 library(geonames)
+library(nasapower)
 options(geonamesUsername="schultem")
 options(geonamesHost="api.geonames.org")
 
-# load dataset
-appointments <- read_csv('_sessions/FinalProject/1_Data/medical_noshows.csv')
 weather <- read_csv('_sessions/FinalProject/1_Data/brazil_wheather.csv')
 
-# remove quotes
-appointments$NoShow <- appointments$`No-show`
-appointments <- select(appointments, -`No-show`)
 
-# Converting all logical flags 
-appointments$Diabetes <- as.logical(appointments$Diabetes)
-appointments$Alcoholism <- as.logical(appointments$Alcoholism)
-appointments$Hypertension <- as.logical(appointments$Hipertension)
-appointments$Handicapped <- as.logical(appointments$Handcap)
-appointments$Scholarship <- as.logical(appointments$Scholarship)
-appointments$SMS_received <- as.logical(appointments$SMS_received)
+# PROCESS APPOINTMENTS ----------
 
+# load dataset
+appointments <- read_csv('_sessions/FinalProject/1_Data/medical_noshows.csv')
+
+# dplyr magic
+appointments = appointments %>%
+  rename(NoShow = "No-show") %>%
+  mutate_if(function(x) all(x %in% c(0, 1)), as.logical)
+  
 # write_csv('_sessions/FinalProject/1_Data/medical_noshows.csv') # run once!
 
-# we need coordinates for the locations in appointments ----
-neighbourhood <- appointments$Neighbourhood 
-unique_neighbourhood <- unique(appointments$Neighbourhood)
+
+# GET COORDINATES -----------
+
+# get unique neighborhoods
+uni_neighbourhood <- unique(appointments$Neighbourhood)
 
 # function to request lon lat from geonames API
 GNsearchAF <- function(x) {  
   res <- GNsearch(name=x, country="BR")  
   return(res[1, ])  
-}
+  }
 
-# loop over city names and reformat  
-GNresult <- sapply(unique_neighbourhood, GNsearchAF)  
-GNresult <- do.call("rbind", GNresult)  
-GNresult <- cbind(city=row.names(GNresult),  
-                  subset(GNresult, select=c("lng", "lat", "adminName1")))
+# # loop over city names and reformat  
+# GNresult <- sapply(uni_neighbourhood, GNsearchAF)  
+# GNresult <- do.call(rbind, GNresult)  
+# GNresult <- cbind(city=row.names(GNresult),  
+#                   GNresult %>% select(lng, lat, adminName1)) %>%
+#                   as.tibble()
+# 
+# write_csv(GNresult, '_sessions/FinalProject/1_Data/latlon_brazil.csv')
 
-write_csv(GNresult, '_sessions/FinalProject/1_Data/LatLonBrazil.csv')
 
-#get weather data
+# GET WHEATHER -----------
 
-library(nasapower)
+GNresult = read_csv('_sessions/FinalProject/1_Data/latlon_brazil.csv')
 
 test_coord <- GNresult[1:3,2:3]
 
@@ -59,15 +61,14 @@ test_coord <- GNresult[1:3,2:3]
 test_coord$lnglat <- noquote(paste0(test_coord$lng, ' ', test_coord$lat))
 c(test_coord$lnglat[1])
 
-out1 <- NULL
-for (i in test_coord$lnglat){
+res <- matrix(nrow=nrow(GNresult), ncol=3)
+for (i in 1:nrow(GNresult)){
   print(i)
-  values <-  get_power(community = "AG",
-                       lonlat = c(i),
+  res[i, ] <-  get_power(community = "AG",
+                       lonlat = c(-47.8821658, -15.7942287), # GNresult %>% slice(i) %>% select(lng, lat) %>% unlist() %>% round(2),
                        pars = c("RH2M", "T2M", "PRECTOT"),
                        dates = c("2016-11-09", "2016-11-10"),
                        temporal_average = "DAILY")
-  out1 <- rbind(out1, values)
                 }    
 
 ?get_power
